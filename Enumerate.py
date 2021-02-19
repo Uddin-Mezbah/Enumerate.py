7b017fed69463c7dddbd534abb52c166d829a445
Mezbah uddin(davide) <75944104+davide666777@users.noreply.github.com>
2021-02-19 23:46:35 +0800

    Add files via upload

---
+++ Enumerate.py
@@ -0,0 +1,13 @@
+n = ["potato","tomato","ladis","lf"]
+
+#i = 1
+#for item in n:
+ #   if i%2 != 0:
+  #      print(f"Jarvise please buy (item)")
+   #i += 1
+
+for index, item in enumerate(n):
+    if index%2==0:
+        print(f"davide please buy(item)")
+
+
